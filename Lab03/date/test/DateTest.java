a8aa8f46c4cc4c10671587e9d0499d8c96226dac
Mary Mousa <mmous102@uottawa.ca>
2021-06-01 23:13:13 -0400

    first part

--- Lab03/date/test/DateTest.java
+++ Lab03/date/test/DateTest.java
@@ -110,6 +110,62 @@ class DateTest {
   }
 
   @Test
+  void nextDate_tc16() { // pour septembre
+    Date today = new Date(2000, 9, 5);
+    Date expectedTomorrow = new Date(2000, 9, 6);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+    @Test
+  void nextDate_leap() { // leap year alors on choisit 2000 pour que ca soit divisible par 400
+    Date today = new Date(2000, 4, 5);
+    Date expectedTomorrow = new Date(2000, 4, 6);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+   @Test
+  void nextDate_2012() { // leap year, on choisit un en fevrier ou il y a 29 jours
+    Date today = new Date(2012, 2, 28);
+    Date expectedTomorrow = new Date(2012, 9, 29);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+     @Test
+  void nextDate_2021() { // not leap year, on choisit un en fevrier ou il n'ya pas 29 jour
+    Date today = new Date(2021, 2, 28);
+    Date expectedTomorrow = new Date(2021, 3, 1);
+    assertEquals(expectedTomorrow, today.nextDate());
+  }
+
+    @Test
+  void nextDate_equals(){ 
+    Object obj = new Object();
+    Date date = new Date(1999,3,21);
+    assertFalse(date.equals(obj));
+}
+
+    @Test
+  void nextDate_equals2(){
+    Date date = new Date(2021,6,28);
+    Date date2 = new Date(2020,8,26);
+    assertFalse(date.equals(date2));
+}
+
+    @Test
+  void nextDate_equals3(){
+    Date date = new Date(2021,6,23);
+    Date date2 = new Date(2021,7,24);
+    assertFalse(date.equals(date2));
+}
+
+    @Test
+void nextDate_equals4(){
+    Date date = new Date(2021,7,20);
+    Date date2 = new Date(2021,7,21);
+    assertFalse(date.equals(date2));
+}
+
+    @Test
   void nextDate_invalid_tc16() {
     assertThrows(
       IllegalArgumentException.class,
@@ -117,8 +173,8 @@ class DateTest {
     );
   }
 
-  @Test
-  void nextDate_invalid_tc17() {
+    @Test
+   void nextDate_invalid_tc17() {
     assertThrows(
       IllegalArgumentException.class,
       () -> new Date(1500, 2, 29)
@@ -148,5 +204,34 @@ class DateTest {
       () -> new Date(1975, 6, -50)
     );
   }
+
+  @Test
+void nextDate_invalid_Day() { // test de plus que 31 jours
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> new Date(1800, 6, 32)
+    );
+}
+
+@Test
+void nextDate_invalid_Day_2() { // test de un mois qui a 30 jours seulement
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> new Date(2001, 4, 31)
+    );
+}
+
+@Test
+void nextDate_invalid_Day_3() { // test de plus que 29 jours en fevrier
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> new Date(2020, 2, 30)
+    );
+}
+@Test
+void nextDate_String(){ // tester la conversion en string de la date
+    Date date = new Date(2019,4,3);
+    assertEquals("2019/April/3",date.toString());
+}
 
 }
